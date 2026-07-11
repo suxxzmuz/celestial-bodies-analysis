863d76a4608cf010c29ebbc7d94f0ad7809ae581
suxxzmuz <wjdtldms4543@gmail.com>
2026-07-11 21:21:57 +0900

    reuplod

--- venv/Lib/site-packages/astropy/cosmology/_src/tests/traits/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Test package for cosmology trait tests."""
-
-__all__ = []
--- venv/Lib/site-packages/astropy/wcs/wcsapi/tests/helpers.py
+++
@@ -1,3 +0,0 @@
-def assert_celestial_component(component, index):
-    assert component[:2] == ("celestial", index)
-    assert callable(component[2])
